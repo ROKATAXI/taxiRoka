3a3f4b9be7bb72037793a9b8037c7d7bcfec89fb
ggom121 <ggom121@naver.com>
2023-08-06 21:12:37 +0900

    feat: 일반로그인기능 구현

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
+    path('main/', views.main, name='main'),
     path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,23 +1,59 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import auth
+from .models import CustomUser 
+from django.contrib.auth.backends import ModelBackend
 
-# Create your views here.
+def main(request):
+    return render(request, 'user/main.html')
+
 def login(request):
-    print("login")
-    if request.method == 'POST' :
+    if request.method == 'POST':
         print("login post")
-
-        id = request.POST['id']
+        user_id = request.POST['id']  
         password = request.POST['password']
 
-        user = auth.authenticate(request, id=id, password=password)
+        user = auth.authenticate(request, username=user_id, password=password)
 
-        if user is None :
+        if user is None:
             print('login fail')
-            return redirect('/login')
-        else :
+            return redirect('/')
+            
+        else:
             auth.login(request, user)
-            user, _ = user.objects.get_or_create(user=user, name=user.username)
+            user, _ = CustomUser.objects.get_or_create(user=user, name=user.username)
+            return redirect('/')
+    return render(request, 'user/login.html') 
+
+def signup(request):   
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        phone = request.POST['phone']
+        location = request.POST['location']
+        in_date = request.POST['in_date']
+        out_date = request.POST['out_date']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+
+        try:
+            existing_user = CustomUser.objects.get(username=username)
+            return redirect('/login/') 
+        except CustomUser.DoesNotExist:
+            user = CustomUser.objects.create_user(
+                username=username,
+                email=email,
+                first_name = first_name,
+                last_name = last_name,
+                password=password,
+                phone=phone,
+                location=location,
+                in_date=in_date,
+                out_date=out_date
+            )
+            user.backend = f'{ModelBackend.__module__}.{ModelBackend.__qualname__}'
+            user.save()
+            auth.login(request, user)
             return redirect('/')
 
-    return render(request, 'user/login.html')
+    return render(request, 'user/signup.html')
