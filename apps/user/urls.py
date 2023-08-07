bc6d520b4efc1811af6ac39b07cd5f1e493abb43
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-08-08 04:57:33 +0900

    feat: kakao로그인 구현 완료(user모델과 연동 미완료)

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'user'
 
 urlpatterns = [
+    path('', views.kakao, name='kakao'),
     path('main/', views.main, name='main'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    
+    path('kakao/redirect/',views.kakao_Auth_Redirect,name="kakaoAuth")
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import auth
 from .models import CustomUser 
 from django.contrib.auth.backends import ModelBackend
+from django.views import View
+import requests
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 def main(request):
     return render(request, 'user/main.html')
@@ -20,7 +25,7 @@ def login(request):
             
         else:
             auth.login(request, user)
-            user = CustomUser.objects.get(username=user.username)
+            user, _ = CustomUser.objects.get_or_create(user=user, name=user.username)
             return redirect('/')
     return render(request, 'user/login.html') 
 
@@ -57,3 +62,66 @@ def signup(request):
             return redirect('/')
 
     return render(request, 'user/signup.html')
+
+
+
+
+
+
+# kakao
+
+
+
+def kakao_Auth_Redirect(request):
+    code = request.GET.get('code',None)
+    print(code)
+    if code:
+        print("code", code)
+        #이제 토큰을 받아야함.
+        headers = {
+        'Content-type':'application/x-www-form-urlencoded;charset=utf-8'
+        }
+        content = {
+        "grant_type": "authorization_code",
+        "client_id": "7e2293a02b5609b94e47fc7bd7929328",
+        "redirect_url": "http://127.0.0.1:8000/user/kakao/redirect",
+        "code": code,
+        }
+        res = requests.post("https://kauth.kakao.com/oauth/token",headers=headers, data=content)
+        print(res.status_code)
+        if res.status_code == 200:
+            print("성공")
+            token_res = res.json()
+            headers123 = {
+                "Authorization" : "Bearer " + token_res['access_token'],
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            }
+            res = requests.post("https://kapi.kakao.com/v2/user/me",headers=headers123)
+            print("res2", res)
+            if res.status_code == 200:
+                profile_res = res.json()
+                username = profile_res['properties']['nickname']
+                id = profile_res['id']
+                user = User.objects.filter(kakaoId=id)
+                if user.first() is not None:
+                    print("로그인")
+                    login(request, user.first(), backend='django.contrib.auth.backends.ModelBackend')
+                    return redirect("/")
+                else:
+                    print("새로 생성")
+                    user = CustomUser()
+                    user.username = username
+                    user.kakaoId = id
+                    user.save()
+                    login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            else:
+                print("user정보 가져오기 실패")
+        else:
+            print("토근발급 실패")
+    
+
+    return redirect("/")
+
+
+def kakao(request):
+   return render(request, 'user/kakao.html')
