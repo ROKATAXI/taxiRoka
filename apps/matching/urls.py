577a57dadd34392086fcc585628522bad903186f
seungmin57 <bsm77125@naver.com>
2023-08-07 22:26:02 +0900

    feat: matchinglist/matching_create 구현

--- apps/matching/urls.py
+++ apps/matching/urls.py
@@ -6,4 +6,5 @@ app_name = 'matching'
 urlpatterns = [
     path('', views.main, name = "main"),
     path('create/', views.matching_create, name="create"),
+    path('apply/<int:pk>', views.matching_apply, name="apply"),
 ]
--- apps/matching/views.py
+++ apps/matching/views.py
@@ -3,9 +3,10 @@ from .models import *
 from apps.user.models import *
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+import datetime
 
-# Create your views here.
-
+# 매칭 방 리스트
+@login_required
 def main(request):
     user_location = request.user.location
     rooms = MatchingRoom.objects.filter(matching__host_yn = True, matching__user_id__location = user_location)
@@ -16,6 +17,8 @@ def main(request):
 
     return render(request, 'matching/matchinglist.html', context=ctx)
 
+# 매칭 방 생성
+@login_required
 def matching_create(request):
     if request.method == 'POST':
         matching_room = MatchingRoom.objects.create(
@@ -37,6 +40,41 @@ def matching_create(request):
             matching_date=timezone.now()
         )
 
-        return redirect('matching/')
+        return redirect('/')
     
     return render(request, "matching/matching_create.html")
+
+# 매칭 신청하기
+@login_required
+def matching_apply(request, pk):
+    matching_room = MatchingRoom.objects.get(id=pk)
+    selected_seats = matching_room.matching.values_list('seat_num', flat = True) #이미 선택된 좌석들
+
+    if request.method == 'POST':
+        user = CustomUser.objects.get(pk=request.user.pk)
+
+        seat_num = request.POST['seat_num']
+        if seat_num in selected_seats:
+            return redirect(f'matching/apply/{matching_room.id}')
+
+        #신청자 Matching객체 생성해주기
+        matching = Matching.objects.create(    
+            matching_room=matching_room,
+            user_id=user,
+            host_yn=False,
+            seat_num=seat_num,
+            matching_date=datetime.now()  
+        )
+
+        #신청자 수 1증가
+        matching_room.current_num += 1
+        matching_room.save()
+
+        return redirect('matching_room_list')
+
+    else:
+        ctx = {
+            'matching_room': matching_room,
+            'selected_seats': selected_seats,
+        }
+        return render(request, 'matching_apply.html', context=ctx)
