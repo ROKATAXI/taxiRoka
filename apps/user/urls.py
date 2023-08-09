2cbb538b90aec6a548a5fb0b42f470183a737ccb
ggom121 <ggom121@naver.com>
2023-08-09 23:43:19 +0900

    fix: 구글 로그인 수정

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('send_email/', views.send_email, name='send_email'),
     path('activate/<int:pk>/', views.activate_account, name='activate_account'),
     # 구글 소셜로그인
-    path('google/login', views.google_login, name='google_login'),
-    path('google/callback/', views.google_callback, name='google_callback'),
+    # path('google/login', views.google_login, name='google_login'),
+    # path('google/callback/', views.google_callback, name='google_callback'),
     path('kakao/redirect/',views.kakao_Auth_Redirect,name="kakaoAuth"),
 ]
