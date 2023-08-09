c341499e2f6c901e3d8c77048436d6344508c208
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-08-10 01:19:36 +0900

    style: 수정

--- taxiRoka/urls.py
+++ taxiRoka/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='matching/')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('chat/', include('apps.chat.urls')),
