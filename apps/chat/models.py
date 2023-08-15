d95b7f267006fb290416bd65d1e546c84ce2f517
hyeonji lee <hyunji3017@naver.com>
2023-08-16 00:35:25 +0900

    fix: 사용자가 이전과 동일한 상태에서 채팅을 진행할 수 있도록 수정

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from apps.user.models import CustomUser as User
+from apps.matching.models import MatchingRoom
 
 # ERD CLOUD 링크: https://www.erdcloud.com/d/zB3wiAaQY7PxnzLyY
 
-class Chatting(models.Model):
-    # 메시지 보낸 유저, 메시지 내용, 메시지 전달 시각
-    msg_sender = models.ForeignKey(User, on_delete=models.CASCADE)
+class Message(models.Model):
+    # 채팅방 식별자, 메시지 보낸 유저, 메시지 내용, 메시지 전달 시각
+    chatting_room_id = models.ForeignKey(MatchingRoom, on_delete=models.CASCADE)
+    msg_sender = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     msg_content = models.TextField()
-    sent_time = models.DateTimeField()
+    sent_time = models.DateTimeField(auto_now_add=True)
